24d07d8cf0aebe220d92aac5a1236ce636831736
Paritosh Pal <paritoshpal7796@gmail.com>
2025-02-03 12:07:03 +0530

    Backed URL 2

--- src/State/Auth/Action.js
+++ src/State/Auth/Action.js
@@ -14,7 +14,7 @@ import {
 } from './ActionType'
 
 // Base API URL (adjust based on your setup)
-const API_BASE_URL = 'http://localhost:8080/api/';
+const API_BASE_URL = 'https://ecommercebackend-production-c7e5.up.railway.app/';
 
 // Action: Register a new user
 export const register = (formData) => async (dispatch) => {
